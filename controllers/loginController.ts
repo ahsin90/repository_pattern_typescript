abca74bf4ec04a060f3b1cdfd00345ba6c452fb3
Muhammad Ahsin <ahsin.and@gmail.com>
2023-02-27 13:47:36 +0700

    login controller

---
+++ controllers/loginController.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { make } from 'simple-body-validator';
+import UsersRepository from '../repositories/UsersRepository';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+interface loginInterface {
+  id: Number;
+  uuid: String;
+  email: String;
+  name: String;
+  token: String;
+  createdAt: String;
+  updatedAt: String;
+}
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    let { email, password } = req.body;
+
+    const rules = {
+      email: 'required|email',
+      password: 'required',
+    };
+
+    const validator = make(req.body, rules);
+
+    if (!validator.validate()) {
+      return res.status(400).json({
+        status: false,
+        message: 'validation error',
+        errors: validator.errors().all(),
+      });
+    }
+
+    const userRepo = new UsersRepository(req);
+    const user = await userRepo.getUserByEmail(email);
+
+    if (user) {
+      // password validation
+      const validPassword = await bcrypt.compare(password, user.password!); // ! 👈️ non-null assertion
+
+      if (!validPassword) {
+        throw 'The email or password invalid';
+      } else {
+        // create JWT token
+        const tokenData = {
+          id: user.id,
+          uuid: user.uuid,
+          email: user.email,
+          name: user.name,
+        };
+
+        let token = jwt.sign(tokenData, process.env.JWT_SECRET_KEY as string, {
+          expiresIn: parseInt(process.env.JWT_EXPIRES_IN as string),
+        });
+
+        let refreshToken = jwt.sign(tokenData, process.env.JWT_REFRESH_TOKEN_KEY as string, {
+          expiresIn: parseInt(process.env.JWT_REFRESH_EXPIRES_IN as string),
+        });
+
+        let resData: any = {};
+        resData = user;
+        resData.password = undefined;
+        resData.token = token;
+        resData.refreshToken = refreshToken;
+
+        res.status(200).json({
+          status: true,
+          data: resData,
+        });
+      }
+    } else {
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ status: false, message: 'Errors!', errors: err });
+  }
+};
